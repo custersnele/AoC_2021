4f958132085479bf37d0f62a853ae53d40e70f72
Nele Custers <custersnele@gmail.com>
2021-12-15 13:15:01 +0100

    day 15 - and now the extra tiles

--- src/main/java/be/ccfun/aoc/day15/Day15Dijkstra.java
+++ src/main/java/be/ccfun/aoc/day15/Day15Dijkstra.java
@@ -21,7 +21,7 @@ public class Day15Dijkstra {
 
 			for (Point point : next.getPoint().neighbours(maxX, maxY)) {
 				if (!visited.contains(point)) {
-					int cost = Integer.parseInt(lines.get(point.getY()).substring(point.getX(), point.getX()+1));
+					int cost = getCost(point);
 					priorityQueue.add(new Node(point, next.getDistance() + cost));
 					if (point.equals(destination)) {
 						return next.getDistance() + cost;
@@ -33,12 +33,34 @@ public class Day15Dijkstra {
 		return 0;
 	}
 
+	private static int getCost(Point point) {
+		int subX = lines.get(0).length();
+		int subY = lines.size();
+		int x = point.getX() % subX;
+		int y = point.getY() % subY;
+		int extraX = point.getX() / subX;
+		int extraY = point.getY() / subY;
+		int value = Integer.parseInt(lines.get(y).substring(x, x+1)) + extraX + extraY;
+		if (value > 9) {
+			value = (value % 10) + 1;
+		}
+		return value;
+	}
+
 	public static void main(String[] arg) throws IOException {
 		lines = Files.readAllLines(Path.of("src/main/resources/day15.txt"));
-		int destX = lines.get(0).length() - 1;
-		int destY = lines.size() - 1;
+		int tiles = 5;
+		int destX = lines.get(0).length() * tiles - 1;
+		int destY = lines.size() * tiles - 1;
 		int curX = 0;
 		int curY = 0;
+
+		for (int y = 0; y <= destY; y++) {
+			for (int x = 0; x <= destX; x++) {
+				System.out.print(getCost(new Point(x, y)));
+			}
+			System.out.println();
+		}
 
 		long result = dijkstra(new Point(curX, curY), new Point(destX, destY), destX, destY);
 		System.out.println(result);
